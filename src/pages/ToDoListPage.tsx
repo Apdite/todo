77f1e41909d7e1a86c1a57d9da0f9995841e2755
APDITE <star171@bk.ru>
2025-10-22 00:22:44 +0300

    H/W #3

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,15 +1,40 @@
+import { useState } from "react"
 import { Form } from "../components/Form/Form.tsx"
 import { Header } from "../components/Header/Header.tsx"
 import { ToDoList } from "../components/ToDoList/ToDoList.tsx"
+import { ToDo } from "../models/todo-item.ts"
 
-export const ToDoListPage = () =>{
-    return(
-        <> 
-    <Header />
-    <Form  />
-    <ToDoList />
+export const ToDoListPage = () => {
+    const [todos, setTodos] = useState<ToDo[]>([])
+
+    const createNewToDo = (text: string) => {
+        const newToDo: ToDo = {
+            id: todos.length,
+            text: text,
+            isDone: false
+        }
+        setTodos([...todos, newToDo])
+    }
+
+    const updateToDo = (toDoItem: ToDo)=>{
+        const newTodos = todos.map((todo) =>{
+            if (todo.id ===  toDoItem.id){
+                todo.isDone = !todo.isDone
+            }
+            return todo
+        })
+        setTodos(newTodos)
+    }
+    const deleteToDo = (toDoItem: ToDo)=>{
+        const newTodos = todos.filter((todo) =>todo.id !== toDoItem.id)
+        setTodos(newTodos)
+    }
+
+    return (
+        <>
+            <Header />
+            <Form createNewToDo={createNewToDo} />
+            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo}/>
         </>
-        
-        
     )
 } 
